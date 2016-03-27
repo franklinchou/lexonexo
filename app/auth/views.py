ece0adad71ca91a373da8242c7acb806b3b30aab
franklin <franklin.chou@yahoo.com>
2016-03-27 00:33:28 -0400

    added last run feature

--- app/auth/views.py
+++ app/auth/views.py
@@ -21,7 +21,8 @@ def login():
         if user is not None and user.verify_password(form.password.data):
             login_user(user)
             return redirect(
-                request.args.get('next') or url_for('public.index')
+                # request.args.get('next') or url_for('public.index')
+                request.args.get('next') or url_for('main.ran')
             )
         # flash()
     return render_template(
